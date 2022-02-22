b69b95011fae858fd12e34939bf19eb4f26f5939
taehyeong park <b_tae96@naver.com>
2022-02-22 15:22:17 +0900

    [update] design in terms
    약관 디자인 업데이트

---
+++ js/all_selected.js
@@ -0,0 +1,20 @@
+function selectAll(selectAll) {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = selectAll.checked
+  })
+}
+
+function checkSelectAll() {
+
+  const checklist = document.querySelectorAll('input[name="checker"]');
+  const checked = document.querySelectorAll('input[name="checker"]:checked');
+  const selectAll = document.querySelector('input[name="selectAll"]');
+  
+  if(checklist.length === checked.length){
+    selectAll.checked = true;
+  }else{
+    selectAll.checked = false;
+  }
+}
