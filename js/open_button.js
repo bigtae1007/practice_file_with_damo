5176acb466cc7a80eded6959c64f20ad50b06031
taehyeong park <b_tae96@naver.com>
2022-02-22 17:08:22 +0900

    [add] 버튼 비활성화 추가

--- js/open_button.js
+++ js/open_button.js
@@ -5,13 +5,20 @@ bt.addEventListener('click', () => {
   const input = document.createElement("input");
   const button = document.createElement("button");
   const text = document.createTextNode("인증하기");
+  const id = document.createAttribute("id");
+  id.value = "next"
 
+  button.setAttributeNode(id);
   button.appendChild(text);
   box.appendChild(input);
   box.appendChild(button);
+
+  
+document.getElementById('next').addEventListener("click",function(){
+  alert("가입완료");
+
+  document.getElementById('bt2').disabled = false;
+})
+
 }, { once : true});
 
-
-document.getElementById('bt2').addEventListener("click",function(){
-  alert("가입완료");
-})
