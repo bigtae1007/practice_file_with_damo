3af6f573b58c3e92ec80bf1829b80d18489ef419
taehyeong park <b_tae96@naver.com>
2022-02-27 15:43:13 +0900

    [update] 배너 페이지 제작
    배너 하단 현재 페이지 나타내도록 수정

--- js/auto._slide.js
+++ js/auto._slide.js
@@ -1,13 +1,22 @@
 function auto_slide(){
   
   const img = document.getElementsByClassName('slide_img');
+  let count = document.getElementById('count');
+  const bt = document.getElementsByClassName('img_bt');
   for(let i = 0 ; i < img.length ; i ++){
     img[i].style.display = "none";
+  }
+  for(let j = 0 ; j < bt.length ; j ++){
+    bt[j].style.backgroundColor = "rgba(68, 63, 63, 0.167)"
   }
   page++;
   if(page > img.length){page = 1}
   img[page - 1].style.display = "block";
-  setTimeout(auto_slide, 10000);
+  bt[page - 1].style.backgroundColor = "rgb(61, 61, 61)";
+  setTimeout(auto_slide, 3000);
+  count.innerHTML = page + " / " + img.length;
 }
+
+
 var page = 0;
 auto_slide();
