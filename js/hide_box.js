0e8a6b482ea85c8ab4707fe957f816cd72a624c0
taehyeong park <b_tae96@naver.com>
2022-03-02 22:22:10 +0900

    [update] 상세페이지 제작
    문의하기 슬라이드 메뉴로 제작 / 좋아요 버튼 활성화 / 가격표시 숨기기 수정

--- js/hide_box.js
+++ js/hide_box.js
@@ -1,15 +1,27 @@
 function display_box() { // 박스 display block or none
   const box = document.querySelectorAll('.price_box');
 
-  if(box[0].style.display === 'none' ){
+  if(box[0].style.display === 'block' ){
     for(let i = 0 ; i  < box.length ; i ++){
-      box[i].style.display = "block";
+      box[i].style.display = "none";
     }
   }else{
     for(let j = 0 ; j < box.length ; j++){
-      box[j].style.display = "none";
+      box[j].style.display = "block";
     }
   }
 }
+
+function display_img(){
+  const img = document.getElementById('heart_icon');
+  if(img.style.display === 'block'){
+    img.style.display = 'none'
+  }else{img.style.display = 'block'
+  }
+}
+
+
+
 const btn = document.getElementById('display_btn');
 btn.addEventListener("click", display_box);
+
---
+++ js/slide_menu.js
@@ -0,0 +1,6 @@
+function open_menu(){
+  document.getElementById('faq_1').style.height = "200px"
+}
+function close_menu(){
+  document.getElementById('faq_1').style.height = "0"
+}
