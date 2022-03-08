3ef3c0177b56931e38fb771e7e03f2faf5c3712f
taehyeong park <b_tae96@naver.com>
2022-03-08 14:28:43 +0900

    [update] 메뉴 선택에 따라 하위 내용 변경
    메뉴 선택에 따라 하위 메뉴 변경

--- js/mult_menu.js
+++ js/mult_menu.js
@@ -1,6 +1,6 @@
 const content = document.querySelectorAll('.menu_button');
 const ACTIVE = "active"
-
+const menu = document.querySelectorAll('.menu_content');
 
 content.forEach(function(contents){
     contents.addEventListener("click",function(){
@@ -9,6 +9,14 @@ content.forEach(function(contents){
             contents.classList.remove(ACTIVE);
         });
         contents.classList.add(ACTIVE);
+
+        for(let i = 0 ; i < content.length ; i ++){
+            if(content[i].classList.contains(ACTIVE) === true){
+                menu[i].classList.add('display');
+            }else{
+                menu[i].classList.remove('display');
+            }
+        }
     })
     // contents.classList.remove(ACTIVE);
 })
