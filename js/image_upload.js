293c8dffdf9d3f040a33cd7381fbc7f6091a506d
taehyeong park <b_tae96@naver.com>
2022-03-11 18:25:24 +0900

    [add] 이미지 업로드 미리보기 js
    이미지 첨부 후 미리보기 추가

---
+++ js/image_upload.js
@@ -0,0 +1,16 @@
+
+function setDetailImage(event){
+  for(var image of event.target.files){
+    var reader = new FileReader();
+    
+    reader.onload = function(event){
+      var img = document.createElement("img");
+      img.setAttribute("src", event.target.result);
+      img.setAttribute("class", "col_lg_6");
+      document.querySelector("div#images_container").appendChild(img);
+    };
+    
+    // console.log(image);
+    reader.readAsDataURL(image);
+  }
+}
